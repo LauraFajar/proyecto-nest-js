aab1f69359ed0f2f47ef8baedd8869bc630a6c28
LauraFajar <lauracamilafajardocalderon@gmail.com>
2025-12-09 21:52:23 -0500

    Cambio en migración de alertas

--- src/database/migrations/1765306200000-AddCreatedAtToAlertas.ts
+++ src/database/migrations/1765306200000-AddCreatedAtToAlertas.ts
@@ -2,10 +2,19 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddCreatedAtToAlertas1765306200000 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            ALTER TABLE "alertas" 
-            ADD COLUMN "created_at" TIMESTAMP DEFAULT now()
+        const tableExists = await queryRunner.query(`
+            SELECT column_name 
+            FROM information_schema.columns 
+            WHERE table_name = 'alertas' 
+            AND column_name = 'created_at'
         `);
+        
+        if (tableExists.length === 0) {
+            await queryRunner.query(`
+                ALTER TABLE "alertas" 
+                ADD COLUMN "created_at" TIMESTAMP DEFAULT now()
+            `);
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
